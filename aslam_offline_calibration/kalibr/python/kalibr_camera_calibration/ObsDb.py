e74d459966408187e474ec9b985895ea5b5742db
Kartik Mohta <kartikmohta@gmail.com>
2016-04-28 16:35:46 -0400

    Use system suitesparse

--- aslam_offline_calibration/kalibr/python/kalibr_camera_calibration/ObsDb.py
+++ aslam_offline_calibration/kalibr/python/kalibr_camera_calibration/ObsDb.py
@@ -21,33 +21,33 @@ import sys
 #
 class ObservationDatabase(object):
     def __init__(self, max_delta_approxsync=0.0):
-        #approximate sync 
+        #approximate sync
         self.max_delta_approxsync = max_delta_approxsync
-        #storage for raw observations 
+        #storage for raw observations
         self.observations = dict()
         #table that connects observations to time (approx sync. work in here...)
         self.targetViews = collections.OrderedDict()
-        
-    def addObservation(self, cam_id, obs):        
+
+    def addObservation(self, cam_id, obs):
         #create camera list (initialization)
         if cam_id not in self.observations:
             self.observations[cam_id] = list()
-            
+
         #add to archive
         self.observations[cam_id].append(obs)
         obs_idx = len(self.observations[cam_id])-1
-    
+
         #find the nearest timestamp in the table
         timestamps_table = self.targetViews.keys()
         timestamp_obs = obs.time().toSec()
-        
+
         #check if the table is still empty (initialization)
         if not timestamps_table:
             #force a new entry (by putting the "nearest" timestamp more than max_delta_approxsync away)
             nearest_timestamp = timestamp_obs + 5*(self.max_delta_approxsync+1)
         else:
-            nearest_timestamp = min(timestamps_table, key=lambda x: abs(x-timestamp_obs))    
-            
+            nearest_timestamp = min(timestamps_table, key=lambda x: abs(x-timestamp_obs))
+
         #if +-max approx. sync add to this time instant otherwise create a new timestamp)
         if abs(nearest_timestamp-timestamp_obs) <= self.max_delta_approxsync:
             #add to existing timestamp
@@ -56,22 +56,22 @@ class ObservationDatabase(object):
             #add new timestamp
             timestamp = timestamp_obs
             self.targetViews[ timestamp ] = dict()
-        
+
         #fill in observation data
         if cam_id not in self.targetViews[timestamp]:
-            #create entry if it doesnt exists           
+            #create entry if it doesnt exists
             self.targetViews[timestamp][cam_id] = dict()
             self.targetViews[timestamp][cam_id]['obs_id'] = obs_idx
             self.targetViews[timestamp][cam_id]['observed_corners'] = set(obs.getCornersIdx())
         else:
             #we already have a view from this camera on this timestamp --> STH IS WRONG
-            sm.logError("[TargetViewTable]: Tried to add second view to a given cameraId & " 
+            sm.logError("[TargetViewTable]: Tried to add second view to a given cameraId & "
                         "timestamp. Maybe try to reduce the approximate syncing tolerance..")
 
 
 #############################################################
 ## data queries
-#############################################################    
+#############################################################
     #returns a list of tuples (cam_id, obs) for all observations at a given time
     def getAllObsAtTimestamp(self, timestamp):
         observations_at_time=[]
@@ -88,24 +88,24 @@ class ObservationDatabase(object):
     #get a list of all timestamps at which we see a target
     def getAllViewTimestamps(self):
         return self.targetViews.keys()
-    
+
     #get a list of all cam_ids that see the target at the given time
     def getCamIdsAtTimestamp(self, timestamp):
         return self.targetViews[timestamp].keys()
-    
+
     #get the observation for given cam_id and timestamp
     def getObservationAtTime(self, timestamp, cam_id):
         obs_id = self.getObsIdForCamAtTime(timestamp, cam_id)
         return self.observations[cam_id][obs_id]
-    
+
     #get the obs_id for a camera at a given time
     def getObsIdForCamAtTime(self, timestamp, cam_id):
         return self.targetViews[timestamp][cam_id]['obs_id']
-    
+
     #get a list of the IDs of all observed corners for a camera at a given time
     def getCornerIdsAtTime(self, timestamp, cam_id):
         return self.targetViews[timestamp][cam_id]['observed_corners']
-    
+
     #return a list of tuples for all observations of a camera pair
     #       list(tuple) = [ (obsA, obsB), (None, obsB), ...]
     #        None if there is no target for a cam in the view
@@ -120,7 +120,7 @@ class ObservationDatabase(object):
                 obsB = self.getObservationAtTime(timestamp, cam_id_B)
             except KeyError:
                 obsB = None
-                
+
             if (obsA is not None) or (obsB is not None):
                 tuples.append( (obsA, obsB) )
         return tuples
@@ -137,20 +137,20 @@ class ObservationDatabase(object):
             if obs is not None:
                 observations.append(obs)
         return observations
-    
+
 #############################################################
 ## data queries
-#############################################################    
+#############################################################
     def printTable(self):
         #header
-        print "timestamp \t",        
+        print "timestamp \t",
         for cam_id in range(0, self.numCameras()):
             print "cam{0} \t".format(cam_id),
         print
-        
+
         #sort for time
         times_sorted = np.sort(self.targetViews.keys())
-        
+
         #data lines
         for time in times_sorted:
             print time,
